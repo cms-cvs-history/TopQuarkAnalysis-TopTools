0c210fffbca1fe262bccdbb208c5287f662a4ae7
Roger Wolf <sha1-bf6c89fe049b9f9a193bed584ffb80166e4ef256@cern.ch>
2009-08-06 00:45:15 +0000

    left over cosmetics after a quest of bug hunting and understanding features

--- src/JetPartonMatching.cc
+++ src/JetPartonMatching.cc
@@ -57,6 +57,7 @@ JetPartonMatching::calculate()
   bool emptyParton=false;
   for(unsigned int ip=0; ip<partons.size(); ++ip){
     if( partons[ip]->pdgId() ==0 ){
+      std::cout << "empty parton index : " << ip << std::endl;
       emptyParton=true;
       break;
     }
